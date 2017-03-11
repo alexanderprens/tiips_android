8198d2564ec8e38a8b4d4816d8c41006ba783f5c
Alexander Rens <alexanderprens@gmail.com>
2017-03-11 13:28:37 -0600

    barcode summary report finished

--- app/src/main/java/org/helpingkidsroundfirst/hkrf/navigation_bar_activities/inventory/view_reports/item_summary/BarcodeSummaryFragment.java
+++ app/src/main/java/org/helpingkidsroundfirst/hkrf/navigation_bar_activities/inventory/view_reports/item_summary/BarcodeSummaryFragment.java
@@ -1,6 +1,7 @@
 package org.helpingkidsroundfirst.hkrf.navigation_bar_activities.inventory.view_reports.item_summary;
 
 
+import android.content.ContentUris;
 import android.database.Cursor;
 import android.net.Uri;
 import android.os.Bundle;
@@ -50,6 +51,8 @@ public class BarcodeSummaryFragment extends Fragment implements
     private TextView categoryView;
     private TextView valueView;
     private TextView barcodeView;
+    private TextView currentQtyView;
+    private TextView pastQtyView;
     private Uri mUri;
 
     public BarcodeSummaryFragment() {
@@ -68,18 +71,17 @@ public class BarcodeSummaryFragment extends Fragment implements
             mUri = bundle.getParcelable(ITEM_URI);
         }
 
-        // assign current text views
-
-        // assign past text views
-
-        // assign detail text views
+        // assign text views
         nameView = (TextView) rootView.findViewById(R.id.item_detail_text_name);
         descriptionView = (TextView) rootView.findViewById(R.id.item_detail_text_description);
         categoryView = (TextView) rootView.findViewById(R.id.item_detail_text_category);
         valueView = (TextView) rootView.findViewById(R.id.item_detail_text_value);
         barcodeView = (TextView) rootView.findViewById(R.id.item_detail_text_barcode);
+        currentQtyView = (TextView) rootView.findViewById(R.id.barcode_summary_current);
+        pastQtyView = (TextView) rootView.findViewById(R.id.barcode_summary_past);
 
         // run current and past data gathering
+        countQty();
 
         return rootView;
     }
@@ -131,5 +133,94 @@ public class BarcodeSummaryFragment extends Fragment implements
     @Override
     public void onLoaderReset(Loader<Cursor> loader) {
 
+    }
+
+    private void countQty() {
+
+        int qtyCurrent = 0;
+        int qtyPast = 0;
+
+        // check if uri exists
+        if (mUri != null) {
+
+            // get item id
+            long itemId = ContentUris.parseId(mUri);
+            String selection = InventoryContract.ItemEntry.TABLE_NAME + "." +
+                    InventoryContract.ItemEntry._ID + " = ? ";
+            String[] selectionArgs = {Long.toString(itemId)};
+
+            // count qty in current
+            Uri countUri = InventoryContract.CurrentInventoryEntry.buildCurrentInventoryUri();
+            String[] currentProjection = {" 1 _id",
+                    InventoryContract.CurrentInventoryEntry.COLUMN_QTY
+            };
+
+
+            Cursor cursor = getContext().getContentResolver().query(
+                    countUri,
+                    currentProjection,
+                    selection,
+                    selectionArgs,
+                    null
+            );
+
+            if (cursor != null && cursor.moveToFirst()) {
+
+                do {
+                    qtyCurrent += cursor.getInt(1);
+                } while (cursor.moveToNext());
+
+                cursor.close();
+            }
+
+            // get item name
+            String itemName = "";
+            cursor = getContext().getContentResolver().query(
+                    mUri,
+                    new String[]{"1 _id",
+                            InventoryContract.ItemEntry.COLUMN_NAME},
+                    null,
+                    null,
+                    null
+            );
+
+            if (cursor != null && cursor.moveToFirst()) {
+
+                // get item name from cursor
+                itemName = cursor.getString(1);
+                cursor.close();
+            }
+
+            if (!itemName.isEmpty()) {
+
+                // count qty in past
+                countUri = InventoryContract.PastInventoryEntry.buildPastInventoryUri();
+                selection = InventoryContract.PastInventoryEntry.COLUMN_NAME + " = ? ";
+                selectionArgs[0] = itemName;
+                String[] pastProjection = {"1 _id",
+                        InventoryContract.PastInventoryEntry.COLUMN_QTY
+                };
+
+                cursor = getContext().getContentResolver().query(
+                        countUri,
+                        pastProjection,
+                        selection,
+                        selectionArgs,
+                        null
+                );
+
+                if (cursor != null && cursor.moveToFirst()) {
+
+                    do {
+                        qtyPast += cursor.getInt(1);
+                    } while (cursor.moveToNext());
+
+                    cursor.close();
+                }
+            }
+        }
+
+        currentQtyView.setText(Integer.toString(qtyCurrent));
+        pastQtyView.setText(Integer.toString(qtyPast));
     }
 }
